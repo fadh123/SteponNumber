d4915657b905d4ed5abd36f98137a0e910bdcc52
fadh123 <80761778+fadh123@users.noreply.github.com>
2021-03-21 16:10:26 +0800

    Add files via upload

---
+++ Soal 2 - Step on Number.py
@@ -0,0 +1,40 @@
+'''
+Jika kita mempunyai starting koordinat di (0,0) dan kita menulis semua bilangan bulat positif seperti gambar
+dibawah. Misalnya, 1, 2, dan 3 ditulis masing-masing pada koordinat (1,1), (2,0), dan (3, 1) dan pola ini
+berlanjut terus menerus.
+'''
+'''
+Buatlah sebuah return function dengan 2 argumen yang dapat membaca koordinat dari titik (x,y) dan
+menuliskan angka (jika ada) yang tertulis pada koordinat yang di-inputkan
+-List Input:
+[[4,2],
+[6,6],
+[3,4]]
+-Function:
+# Function Initialization
+def steponNumber(...):
+ .....
+ 
+# Use the Function
+print(steponNumber(List_awal))
+
+-List Output:
+[6, 12, No Number]
+'''
+#Input Nilai Koordinat
+A = [[6,7,8,9,10,11,12,13],
+    [4,5,6,7,8,9,10,11],
+    [4,5,6,7,8,9,10,11],
+    [2,3,4,5,6,7,8,9],
+    [2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7],
+    [0,1,2,3,4,5,6,7]]
+    
+#Print 
+print(A[4][2])
+print(A[6][6])
+print(A[3][4])
+
+
+
+
